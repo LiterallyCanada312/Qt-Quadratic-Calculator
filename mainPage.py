301fc463ba4c15f18d2f82133016f245de21eec3
Ben <whippedcreamtoast@gmail.com>
2023-09-03 14:12:48 -0400

    did more window stuff

--- mainPage.py
+++ mainPage.py
@@ -3,17 +3,39 @@ import mathStuff
 
 from PySide6.QtCore import Qt, Slot
 from PySide6.QtWidgets import (QApplication, QLabel, QPushButton,
-                               QVBoxLayout, QWidget)
+                               QVBoxLayout, QWidget, QLineEdit)
+from PyQt6.QtGui import (QDoubleValidator)
 
 class mainPage:
 
     def __init__ (self):
         QWidget.__init__(self)
 
+        aInput = QLineEdit()
+        aInput.setValidator(QDoubleValidator())
+
+        bInput = QLineEdit()
+        bInput.setValidator(QDoubleValidator())
+
+        cInput = QLineEdit()
+        cInput.setValidator(QDoubleValidator())
+        
+
+        form = QVBoxLayout()
+        form.addItem(aInput)
+        form.addItem(bInput)
+        form.addItem(cInput)
+
+        self.setLayout(form)
+        self.setWindowTitle("Homework Helper")
 
     @Slot()
     def quadraticPressed(self):
         mathStuff.doQuadratic()
 
 
-    
+if __name__ == 'main':
+    app = QApplication(sys.argv)
+    window = mainPage()
+    window.show()
+    sys.exit(app.exec())
